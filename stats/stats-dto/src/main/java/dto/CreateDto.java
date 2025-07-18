0ac30f4b2b817ef6591f0e4b7290f29866b03f54
Just Roma <romke411@gmail.com>
2025-07-18 14:12:14 +0500

    fix 2

--- stats/stats-dto/src/main/java/dto/CreateDto.java
+++ stats/stats-dto/src/main/java/dto/CreateDto.java
@@ -3,6 +3,7 @@ package dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -23,7 +24,7 @@ public class CreateDto {
     private String ip;
 
     @NotNull
-    @FutureOrPresent
+    @Past
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime timestamp;
 
